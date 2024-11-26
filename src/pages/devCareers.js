8bdb79ff7dd7799ee8510c51500288440be70100
Alyssa Ngo <amaengo@protonmail.com>
2024-11-26 10:45:53 +1000

    Added icons representing each result in devCareers page and made slight visual fixes to make it presentable and readable to users

--- src/pages/devCareers.js
+++ src/pages/devCareers.js
@@ -2,6 +2,20 @@
 import { Card, CardImg, CardText, CardTitle } from 'reactstrap';
 import { useEffect, useState } from 'react';
 import { Grid2 } from '@mui/material';
+import { HiMiniCodeBracket } from "react-icons/hi2";
+import { PiBracketsCurly, PiInfinityBold } from 'react-icons/pi';
+import { FiDatabase } from "react-icons/fi";
+import { ImStack } from "react-icons/im";
+import { BiJoystick } from "react-icons/bi";
+
+const iconMapping = {
+    "Front-End Developer": <HiMiniCodeBracket size={70} />, 
+    "Back-End Developer": <PiBracketsCurly size={70} />, 
+    "Full Stack Developer": <ImStack size={70} />, 
+    "DevOps Engineer": <PiInfinityBold size={70} />, 
+    "Database Administrator": <FiDatabase size={70} />, 
+    "Game Developer": <BiJoystick size={70} />
+}; 
 
 //This page lists all possible results from taking the test
 export function DevCareers() {
@@ -34,6 +48,7 @@ export function DevCareers() {
     return (
         <>
             <h2>Careers</h2>
+            <p><em>These are all the possible results from taking the test.</em></p>
             <Grid2 className="gridContainer" container spacing={2}>
                 {
                     /* Place each career and short description in a card
@@ -43,6 +58,7 @@ export function DevCareers() {
                             <Grid2 size={4}>
                                 <Card className="careerCard">
                                     <CardTitle>{career.career_name}</CardTitle>
+                                    <CardText>{iconMapping[career.career_name] || " "}</CardText>
                                     <CardText className='cardText'>{career.personality_description}</CardText>
                                 </Card>
                             </Grid2>
